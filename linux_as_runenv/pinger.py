320e5c43cb13e96bc2f92a4d31611e51d069aad8
5tuffy13 <sashashem25052006@gmail.com>
2026-02-25 07:50:32 +0800

    added .venv to .gitignore, added delay to requests in pinger.py

--- linux_as_runenv/pinger.py
+++ linux_as_runenv/pinger.py
@@ -1,10 +1,15 @@
 import requests
 from datetime import datetime, timedelta
+import time
+import logging
 url_template = "https://simurg.space/gen_file?data=obs&date={date}"
 
 day_offset = 1
+delay_between_requests = 5
+max_attempts = 30
 
-while True:
+
+while day_offset < max_attempts:
  
     yesterday = datetime.now() - timedelta(days=day_offset)
     yesterday = yesterday.strftime("%Y-%m-%d")
@@ -20,4 +25,8 @@ while True:
     else:
         print(f"Failed to get data for {yesterday}")
 
-    day_offset +=1
+    day_offset += 1
+    time.sleep(delay_between_requests)
+
+if day_offset > max_attempts:
+    logging.error("Failed to retrieve data after maximum attempts")
